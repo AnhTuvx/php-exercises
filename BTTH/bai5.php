a2c775c1ed27ecbddd472a4af2931bb163b0e858
AnhTuvx <zzkuxinzz@gmail.com>
2024-10-15 20:04:53 +0700

    finish 2-8 php

--- BTTH/bai5.php
+++ BTTH/bai5.php
@@ -1,77 +1,82 @@
-<!DOCTYPE html>
-<html>
+<!doctype html>
+<html lang="en">
 <head>
-    <title>Form Tính Tiền</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Form</title>
 </head>
 <body>
 <?php
-$tongTien = null;
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Lấy dữ liệu từ form và chuyển đổi thành đối tượng DateTime
-    $gioBatDau = new DateTime($_POST["gioBatDau"]);
-    $gioKetThuc = new DateTime($_POST["gioKetThuc"]);
-
-    // Khởi tạo thời điểm bắt đầu và kết thúc của các khung giờ
-    $gioBatDauKhung1 = new DateTime('10:00:00');
-    $gioKetThucKhung1 = new DateTime('17:00:00');
-    $gioBatDauKhung2 = new DateTime('17:00:00');
-    $gioKetThucKhung2 = new DateTime('24:00:00');
-
-    // Kiểm tra xem thời gian có nằm trong khung giờ nào không
-    if ($gioBatDau >= $gioBatDauKhung1 && $gioKetThuc <= $gioKetThucKhung1) {
-        // Nếu nằm trong khung giờ 1
-        $tongSoGiay = $gioKetThuc->getTimestamp() - $gioBatDau->getTimestamp();
-        $donGiaGio1 = 20000 / 3600; // Đơn giá cho khung giờ 10h-17h
-        $tongTien = $tongSoGiay * $donGiaGio1;
-    } elseif ($gioBatDau >= $gioBatDauKhung2 && $gioKetThuc <= $gioKetThucKhung2) {
-        // Nếu nằm trong khung giờ 2
-        $tongSoGiay = $gioKetThuc->getTimestamp() - $gioBatDau->getTimestamp();
-        $donGiaGio2 = 45000 / 3600; // Đơn giá cho khung giờ 17h-24h
-        $tongTien = $tongSoGiay * $donGiaGio2;
-    } elseif ($gioBatDau < $gioKetThucKhung1 && $gioKetThuc > $gioBatDauKhung2) {
-        // Nếu thời gian bắt đầu trước 17h và kết thúc sau 17h
-        $soGiayKhung1 = ($gioKetThucKhung1->getTimestamp() - $gioBatDau->getTimestamp());
-        $soGiayKhung2 = ($gioKetThuc->getTimestamp() - $gioBatDauKhung2->getTimestamp());
-
-        $donGiaGio1 = 20000 / 3600;
-        $donGiaGio2 = 45000 / 3600;
-
-        $tongTien = ($soGiayKhung1 * $donGiaGio1) + ($soGiayKhung2 * $donGiaGio2);
-    } else {
-        // Nếu thời gian không nằm trong khung giờ hoạt động
-        echo "Quán không hoạt động.";
+if(isset($_POST['submit'])){
+    $start = $_POST['start'];
+    $end = $_POST['end'];
+    if(isset($start) and isset($end)){
+        $s_hour = intval(substr($start, 0,2));
+        $s_min = intval(substr($start, 2,2));
+        $e_hour = intval(substr($end, 0,2));
+        $e_min = intval(substr($end, 2,2));
+        if($s_hour <10 && $e_hour >0){
+            $tien = "Chứa thời gian nghỉ";
+        }
+        else{
+            if($e_hour < $s_hour || ($e_hour == $s_hour && $e_min > $s_min)){
+                $tien = "Giờ kết thúc phải > Giờ bắt đầu";
+            }
+            else{
+                if($e_min < $s_min){
+                    $e_hour--;
+                    $e_min+=60;
+                }
+                if($e_hour >= 17){
+                    if($s_hour >= 17){
+                        $tien = ($e_hour - $s_hour) * 45000 + ($e_min - $s_min)*45000/60;
+                    }
+                    else{
+                        $tien = ($e_hour - 17) * 45000 + $e_min*45000/60 + (17 - $s_hour) * 20000 + (60 - $s_min)*20000/60;
+                    }
+                }
+                else {
+                    $tien = ($e_hour - $s_hour) * 20000 + ($e_min - $s_min)*20000/60;
+                }
+            }
+        }
+    }
+    else{
+        $tien = "Sai định dạng";
+    }
+    if(isset($_POST['reset'])){
+        $start = '';
+        $end = '';
+        $tien = '';
     }
 }
 ?>
-
-<form action="" name="MyForm_RECT" method="post">
-        <table style= "background-color:lightpink" align="center">
-            <tr>
-                <td style="background-color:DeepPink" align="center" colspan="2"><h3>TINH TIEN KARAOK</h3></td>
-            </tr>
-                <td><label for="gioBatDau">Giờ bắt đầu:</label></td>
-                <td>
-                <input type="time" id="gioBatDau" name="gioBatDau" required>
-                </td>
-            </tr>
-            <tr>
-                <td><label for="gioKetThuc">Giờ kết thúc:</label></td>
-                <td>
-                <input type="time" id="gioKetThuc" name="gioKetThuc" required>
-                </td>
-            </tr>
-            <tr>
-                <td>Ket Qua</td>
-                <td>
-                    <input type="text" name="tongTien" value="<?php echo $tongTien; ?>" size="30" style="background-color: antiquewhite;" readonly>
-                </td>
-            </tr>
-            <tr>
-                <td colspan="2" align="center">
-                    <input type="submit" name="tinh" value="Xem Ket Qua">
-                </td>
-            </tr>
-        </table>
-    </form>
+<form action="" name="tienKaraoke" method="post">
+    <table align="center" style="background-color: cadetblue">
+        <tr>
+            <td style="background-color: teal; color: white; text-align: center" colspan="3"><h4>TÍNH TIỀN KARAOKE</h4></td>
+        </tr>
+        <tr>
+            <td>Giờ bắt đầu:</td>
+            <td><input type="time" name="start" value="<?php if (isset($start)) echo $start; else echo ''?>"></td>
+            <td>(h)</td>
+        </tr>
+        <tr>
+            <td>Giờ kết thúc:</td>
+            <td><input type="time" name="end" value="<?php if (isset($end)) echo $end; else echo ''?>"></td>
+            <td>(h)</td>
+        </tr>
+        <tr>
+            <td>Tiền thanh toán:</td>
+            <td><input type="text" name="tien" readonly value="<?php if (isset($tien)) echo $tien; else echo ''; ?>" style="background-color: lightyellow"></td>
+        </tr>
+        <tr>
+            <td colspan="3" style="text-align: center">
+                <input type="submit" name="submit" value="Tính tiền">
+                <input type="submit" name="reset" value="Reset">
+            </td>
+        </tr>
+    </table>
+</form>
 </body>
 </html>
